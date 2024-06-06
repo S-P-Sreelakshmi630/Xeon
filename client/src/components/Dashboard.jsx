1837d2a5437d5c38331e340c8fa85a08b6db16de
saikrishna1506 <108617880+saikrishna1506@users.noreply.github.com>
2024-06-07 00:07:13 +0530

    Added HeaderBox of DashBoard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,19 @@
-
-const Dashboard = () => {
+import React from 'react';
+//HeaderBox
+const Dashboard = ({ type = "title", title, subtext, user }) => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">
+            &nbsp;{user}
+          </span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
