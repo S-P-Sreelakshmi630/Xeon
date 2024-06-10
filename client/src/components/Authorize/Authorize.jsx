b812760091bbd2758ca23d65f6393639037f208e
KotaDinesh04 <mail.kotadinesh@gmail.com>
2024-06-10 22:44:00 +0530

    Fixed all the styling bugs in authorize components

--- client/src/components/Authorize/Authorize.jsx
+++ client/src/components/Authorize/Authorize.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import Sign_IN from "./Sign_IN";
-import Sign_UP from "./Sign_UP";
+// import Sign_UP from "./Sign_UP";
 import image from '../../icons/auth.jpg';
 import './Authorise.css';
 import LoadingBar from 'react-top-loading-bar';
@@ -18,11 +18,11 @@ const Authorize = () => {
   };
 
   return (
-    <section className="body ">
+    <section className="body-auth">
       <LoadingBar ref={loadingBarRef} color="#64b6fa" height={5} />
-      <div className="main-container">
-        <div className="signIn-text">
-          {flag ? <Sign_UP /> : <Sign_IN onSignUpClick={handleSignUpClick} />}
+      <div className="main-container-auth">
+        <div className="signIn-text-auth">
+          <Sign_IN/>
         </div>
         <div className="image-container">
           <img src={image} className="image sidebar" alt="Auth" />
--- client/src/components/Authorize/Sign_UP.jsx
+++ client/src/components/Authorize/Sign_UP.jsx
@@ -1,173 +1,195 @@
-import "./Authorise.css";
+// import "./Authorise.css";
 import "./Sign_UP.css";
-import axios from 'axios'
-import {Link, useNavigate} from 'react-router-dom'
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-
-
-//Deleted some fields which are of no use!
+import image from "../../icons/auth.jpg";
 
 const Sign_UP = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [state, setState] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [dob, setDob] = useState("");
+  const [ssn, setSsn] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const navigate = useNavigate();
+  axios.defaults.withCredentials = true;
 
-  const [name, setName] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [errorMessage, setErrorMessage] = useState(""); // Add state for error message
-
-const navigate = useNavigate()
-axios.defaults.withCredentials=true;
   const handleSubmit = (e) => {
-    console.log(name);
-
-      e.preventDefault()
-      //Backend server
-      axios.post('http://localhost:3001/signup', {name, email, password})
-
-      .then(res => {
-          navigate('/');
-      }).catch(err => {
-        console.log(err)
-        setErrorMessage("An error occurred: " + err.message); // Set error message
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/signup", { name, lastName, address, state, postalCode, dob, ssn, email, password })
+      .then((res) => {
+        navigate("/");
       })
-  }
-
-
-
-
-
-
-
+      .catch((err) => {
+        console.log(err);
+        setErrorMessage("An error occurred: " + err.message);
+      });
+  };
 
   return (
-    <form className="main " onSubmit={handleSubmit} >
-      <b>
-        <h1 style={{ fontSize: "35px", marginTop: "10%" }}>Sign Up</h1>
-      </b>
-      <p style={{ marginTop: "5px" }}>Please enter your details.</p>
-      <div className="names-container">
-        <p className="names" name="name" style={{ marginTop: "30px" }}>
-          First Name
-        </p>
-        {/* <p
-          className="names last-name"
-          style={{ marginTop: "30px", paddingLeft: "330px" }}
-        >
-          Second Name
-        </p> */}
+    <section className="body">
+      <div className="main-container">
+        <div className="form-container">
+          <form className="main signIn-text" onSubmit={handleSubmit}>
+            <b>
+              <h1 style={{ fontSize: "35px" }}>Sign Up</h1>
+            </b>
+            <p style={{ marginTop: "5px" }}>Please enter your details.</p>
+            <div className="names-container">
+              <p className="names" style={{ marginTop: "30px" }}>
+                First Name
+              </p>
+              <p
+                className="names last-name"
+                style={{ marginTop: "30px", paddingLeft: "330px" }}
+              >
+                Second Name
+              </p>
+            </div>
+            <div className="forms">
+              <input
+                style={{paddingRight:"50px"}}
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="ex: John"
+                aria-label=".form-control-lg example"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className="form-control form-control-lg last-name"
+                type="text"
+                placeholder="ex: Doe"
+                aria-label=".form-control-lg example"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Address</p>
+            <div className="address-form">
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="Enter your specific address"
+                aria-label=".form-control-lg example"
+                required
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+            <div className="names-container">
+              <p className="names" style={{ marginTop: "30px" }}>
+                State
+              </p>
+              <p
+                className="names last-name"
+                style={{ marginTop: "30px", paddingLeft: "370px" }}
+              >
+                Postal Code
+              </p>
+            </div>
+            <div className="forms">
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="ex: Telangana"
+                aria-label=".form-control-lg example"
+                onChange={(e) => setState(e.target.value)}
+              />
+              <input
+                className="form-control form-control-lg last-name"
+                type="text"
+                placeholder="ex: 500083"
+                aria-label=".form-control-lg example"
+                
+                onChange={(e) => setPostalCode(e.target.value)}
+              />
+            </div>
+            <div className="names-container">
+              <p className="names" style={{ marginTop: "30px" }}>
+                Date of Birth
+              </p>
+              <p
+                className="names last-name"
+                style={{ marginTop: "30px", paddingLeft: "330px" }}
+              >
+                SSN
+              </p>
+            </div>
+            <div className="forms">
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="ex: dd-mm-yyyy"
+                aria-label=".form-control-lg example"
+                // required
+                onChange={(e) => setDob(e.target.value)}
+              />
+              <input
+                className="form-control form-control-lg last-name"
+                type="text"
+                placeholder="ex: 1234"
+                aria-label=".form-control-lg example"
+                
+                onChange={(e) => setSsn(e.target.value)}
+              />
+            </div>
+            <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Email ID</p>
+            <div className="address-form">
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="Enter your email"
+                aria-label=".form-control-lg example"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Password</p>
+            <div className="address-form">
+              <input
+                className="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                aria-label=".form-control-lg example"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="submit text-center">
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg submit-btn"
+              >
+                Sign Up
+              </button>
+            </div>
+            <p className="no-style" style={{ paddingRight: "100px" }}>
+              Already have an account?{" "}
+              <Link to="/" color="blue">
+                Login
+              </Link>
+            </p>
+            {errorMessage && (
+              <p className="error-message" style={{ color: "red" }}>
+                {errorMessage}
+              </p>
+            )}
+          </form>
+        </div>
+        <div className="image-container">
+          <img src={image} className="image" alt="Auth" />
+        </div>
       </div>
-      <div className="forms">
-        <input
-          className="form-control form-control-lg"
-          type="text"
-          placeholder="Enter Name"
-          name="name"
-          aria-label=".form-control-lg example"
-          required
-          onChange={(e)=>setName(e.target.value)}
-        />
-        {/* <input
-          className="form-control form-control-lg last-name"
-          type="text"
-          placeholder="ex: Doe"
-          aria-label=".form-control-lg example"
-          required
-        /> */}
-      </div>
-      <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Address</p>
-      <div className="address-form">
-        <input
-          className="form-control form-control-lg"
-          type="text"
-          placeholder="Enter your specific address"
-          aria-label=".form-control-lg example"
-          required
-        />
-      </div>
-      <div className="names-container">
-        <p className="names" style={{ marginTop: "30px" }}>
-         
-        </p>
-        <p
-          className="names last-name"
-          style={{ marginTop: "30px", paddingLeft: "370px" }}
-        >
-          Postal Code
-        </p>
-      </div>
-      <div className="forms">
-        {/* <input
-          className="form-control form-control-lg"
-          type="text"
-          placeholder="ex: Telangana"
-          aria-label=".form-control-lg example"
-          required
-        /> */}
-        {/* <input
-          className="form-control form-control-lg last-name"
-          type="text"
-          placeholder="ex: 500083"
-          aria-label=".form-control-lg example"
-          required
-        /> */}
-      </div>
-      <div className="names-container">
-        <p className="names" style={{ marginTop: "30px" }}>
-          
-        </p>
-        <p
-          className="names last-name"
-          style={{ marginTop: "30px", paddingLeft: "330px" }}
-        >
-          
-        </p>
-      </div>
-      <div className="forms">
-        {/* <input
-          className="form-control form-control-lg"
-          type="text"
-          placeholder="ex: dd-mm-yyyy"
-          aria-label=".form-control-lg example"
-          required
-        /> */}
-        {/* <input
-          className="form-control form-control-lg last-name"
-          type="text"
-          placeholder="ex: 1234"
-          aria-label=".form-control-lg example"
-          required
-        /> */}
-      </div>
-      <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Email ID</p>
-      <div className="address-form">
-        <input
-          className="form-control form-control-lg"
-          type="text"
-          name="email"
-          placeholder="Enter your email"
-          aria-label=".form-control-lg example"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <p style={{ paddingTop: "15px", paddingBottom: "10px" }}>Password</p>
-      <div className="address-form">
-        <input
-          className="form-control form-control-lg"
-          type="text"
-          placeholder="Password"
-          aria-label=".form-control-lg example"
-          required
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="submit text-center">
-        <button  type="submit"  className="btn btn-primary btn-lg submit-btn">Sign Up</button>
-      </div>
-      <p className="no-style" style={{paddingRight:"100px"}}>Already have an account? <a href="/" color="blue">Login</a></p>
-    </form>
+    </section>
   );
 };
 
 export default Sign_UP;
+
+
